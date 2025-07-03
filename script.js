85a6f8cb80de08863525efb49cca8ebbe60e4b78
harshh-2 <harsh2dubey@gmail.com>
2025-07-03 22:06:59 +0530

    basic required functionality achieved (MVP)

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+window.onload=()=>{
+creategrid(5);
+};
 let res=document.getElementById("reset");
 let pop=document.getElementById("popup");
 let col=document.getElementById("picker");
@@ -19,8 +22,26 @@ function grid(){
 }
 
 function creategrid(size){
-
-  
+    let gri=document.getElementById("grid");
+    gri.innerHTML=``;
+    gri.style.display="grid";
+    gri.style.width="600px";
+    gri.style.height="600px";
+    gri.style.gap="1px";
+    gri.style.marginTop="20px";
+    gri.style.gridTemplateColumns=`repeat(${size}, 1fr)`;
+    gri.style.gridTemplateRows=`repeat(${size}, 1fr)`;
+    for(let i=0;i<size*size;i++){
+        let div=document.createElement("div");
+        div.classList.add("grid");
+        div.style.border="2px solid black";
+        div.style.width="100%";
+        div.style.height="100%";
+        div.addEventListener("click",()=>{
+            div.style.backgroundColor="black";
+        })
+        gri.appendChild(div);
+    }
 }
 
 
